1c69ecee0c986d2f155f60779a239f71b0f105dc
Juliana0203 <j.morenon2@uniandes.edu.co>
2026-04-30 15:17:46 -0500

    fit: Interfaz UI

--- tickets/forms.py
+++ tickets/forms.py
@@ -4,4 +4,4 @@ from .models import Ticket
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['título', 'descripción', 'categoria']
+        fields = ['titulo', 'descripcion', 'categoria']
